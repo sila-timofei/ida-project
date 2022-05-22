cf3d42a4d85a2c4df50dc96a2506327dd4cf25ea
Timofei <sila.timofey@gmail.com>
2022-05-23 02:46:40 +0300

    add form validation

--- src/components/AddProductForm.vue
+++ src/components/AddProductForm.vue
@@ -1,24 +1,75 @@
 <template>
   <form class="form">
     <div class="form__field-label">Наименование товара</div>
-    <input type="text" class="form__field" placeholder="Введите наименование товара">
+    <input type="text" class="form__field" placeholder="Введите наименование товара"
+           @input="validateForm"
+           v-model="name"
+           :class="{ error: !isNameValidated }"
+    >
 
     <div class="form__field-label form__label-not-necessary">Описание товара</div>
     <textarea class="form__field form__big-field" id="" placeholder="Введите описание товара"></textarea>
 
     <div class="form__field-label">Ссылка на изображение товара</div>
-    <input type="text" class="form__field" placeholder="Введите ссылку">
+    <input type="text" class="form__field"
+           placeholder="Введите ссылку"
+           @input="validateForm"
+           v-model="imageUrl"
+           :class="{ error: !isImageUrlValidated }"
+    >
 
     <div class="form__field-label">Цена товара</div>
-    <input type="text" class="form__field" placeholder="Введите цену">
+    <input type="text" class="form__field" placeholder="Введите цену"
+           @input="validateForm"
+           v-model="price"
+           :class="{ error: !isPriceValidated }"
+    >
 
-    <button type="button" class="form__btn-send form__btn-send_active">Добавить товар</button>
+    <button type="button" class="form__btn-send"
+            :class="{ active: isActive }"
+    >Добавить товар
+    </button>
   </form>
 </template>
 
 <script>
 export default {
-  name: "AddProductForm"
+  name: "AddProductForm",
+  data() {
+    return {
+      name: '',
+      isNameValidated: true,
+      description: '',
+      imageUrl: '',
+      isImageUrlValidated: true,
+      price: '',
+      isPriceValidated: true,
+      isActive: false
+    }
+  },
+  methods: {
+    validateForm() {
+      this.isNameValidated = this.name.length > 0
+      this.isImageUrlValidated = this.imageUrl.length > 0
+      this.isPriceValidated = this.price.length > 0
+
+      if (this.name.length > 0 &&
+          this.imageUrl.length > 0 &&
+          this.price.length > 0) {
+        this.isActive = true
+      } else  {
+        this.isActive = false
+
+        if (this.name.length === 0 &&
+            this.imageUrl.length === 0 &&
+            this.price.length === 0) {
+          this.isNameValidated = true
+          this.isImageUrlValidated = true
+          this.isPriceValidated = true
+        }
+      }
+    }
+  }
 }
 </script>
 
@@ -55,10 +106,14 @@ export default {
   outline: none;
   border: 1px solid transparent;
   transition: 0.4s;
-}
 
-.form__field:focus {
-  box-shadow: 0 2px 10px rgba(23, 118, 227, 0.26);
+  &:focus {
+    box-shadow: 0 2px 10px rgba(23, 118, 227, 0.26);
+  }
+
+  &.error {
+    border: 1px solid #FF8484;
+  }
 }
 
 .form__big-field {
@@ -66,10 +121,6 @@ export default {
   width: 100%;
   max-width: 100%;
   font-family: "Arial", sans-serif;
-}
-
-.form__field_error {
-  border: 1px solid #FF8484;
 }
 
 .form__field-label {
@@ -113,24 +164,24 @@ export default {
   cursor: not-allowed;
   margin-top: 14px;
   transition: 0.2s;
-}
 
-.form__btn-send_active {
-  background-color: #7BAE73;
-  color: #FFFFFF;
-  cursor: pointer;
-}
+  &.active {
+    background-color: #7BAE73;
+    color: #FFFFFF;
+    cursor: pointer;
 
-.form__btn-send_active:hover {
-  background-color: #5ca84b;
-}
+    &:hover {
+      background-color: #5ca84b;
+    }
 
-.form__btn-send_active:focus:not(:active) {
-  box-shadow: 0 0 0 0.125em rgb(92, 168, 75);
-}
+    &:focus:not(:active) {
+      box-shadow: 0 0 0 0.125em rgb(92, 168, 75);
+    }
 
-.form__btn-send_active:focus {
-  border-color: transparent;
-  color: #fff;
+    &:focus {
+      border-color: transparent;
+      color: #fff;
+    }
+  }
 }
 </style>
